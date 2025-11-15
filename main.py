0a4e323740ee54c14dc7e4bedfe9a6c6072a6a9d
svechkinn <vavinrtik@gmail.com>
2025-11-15 23:20:41 +0500

    Fix: import types from aiogram

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from aiogram.types import (
     FSInputFile, BufferedInputFile,
 )
 from aiogram.filters import Command
-from aiogram import Bot, Dispatcher, F
+from aiogram import Bot, Dispatcher, F, types
 
 import db   # импортируем наш модуль работы с базой
 
