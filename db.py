86911bc1247f0b11352bb4b75c3d0c4b046c6e99
svechkinn <vavinrtik@gmail.com>
2025-11-15 19:53:50 +0500

    Add db.py with init_db function

---
+++ db.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+def init_db():
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        user_id INTEGER PRIMARY KEY,
+        username TEXT,
+        first_name TEXT
+    )
+    """)
+    conn.commit()
+    conn.close()
